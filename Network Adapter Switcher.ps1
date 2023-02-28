21452b6e4ffc6d3b48735189670ac4edb63f175a
moon1z10 <blue_arena@naver.com>
2023-02-28 21:08:54 +0900

    Upload a first draft file
    Network swithcer developed in Powershell script.
    - Need an administrator administrator privileges for executing.
    - It swtiches between "Ethernet" and "Wi-Fi" in Windows

---
+++ Network Adapter Switcher.ps1
@@ -0,0 +1,32 @@
+﻿# Set the names of the Ethernet and Wi-Fi adapters
+$adapterNameEthernet = "이더넷"
+$adapterNameWiFi = "Wi-Fi"
+
+# Get the current adapter status
+$currentAdapter = Get-NetAdapter |
+    Where-Object { $_.Status -eq "Up" } |
+    Select-Object -First 1 |
+    ForEach-Object {
+        if ($_.Name -eq $adapterNameEthernet) { "Ethernet" }
+        elseif ($_.Name -eq $adapterNameWiFi) { "Wi-Fi" }
+    }
+
+# Switch the adapter
+if ($currentAdapter -eq "Ethernet") {
+    Write-Output "Ethernet adapter is currently enabled"
+    Disable-NetAdapter -Name $adapterNameEthernet -Confirm:$false
+    Start-Sleep -Seconds 2
+    Enable-NetAdapter -Name $adapterNameWiFi -Confirm:$false
+    Write-Output "Wi-Fi adapter is now enabled"
+}
+elseif ($currentAdapter -eq "Wi-Fi") {
+    Write-Output "Wi-Fi adapter is currently enabled"
+    Disable-NetAdapter -Name $adapterNameWiFi -Confirm:$false
+    Start-Sleep -Seconds 2
+    Enable-NetAdapter -Name $adapterNameEthernet -Confirm:$false
+    Write-Output "Ethernet adapter is now enabled"
+}
+else {
+    Write-Output "Could not find Ethernet or Wi-Fi adapter"
+    Pause
+}
